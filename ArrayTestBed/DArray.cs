f108655f10007005dbacfd072641d431a8a5bdd3
sazuxerox <sazzadhossain@live.com>
2016-01-05 00:47:49 +0600

    A new class created to implement selection sort. All the things are the same. But the method for selection sort is a little bit different with the method of bubblesort.

---
+++ ArrayTestBed/DArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ArrayTestBed
+{
+    class DArray
+    {
+        //initializing the properties
+        private readonly int[] _array;
+        private readonly int _upper;
+        private int _numElements;
+
+
+        //constructor
+        //initializing the variables inside the constructor
+        public DArray(int size)
+        {
+            _array = new int[size];
+            _upper = size - 1;
+            _numElements = 0;
+        }
+
+        //insert elements into the array
+        public void Insert(int item)
+        {
+            _array[_numElements] = item;
+            _numElements++;
+        }
+
+        //display the array element.
+        public void DisplayElements()
+        {
+            for (int i = 0; i <= _upper; i++)
+            {
+                Console.Write(_array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        //clear the array value
+        public void Clear()
+        {
+            //traversing all the values of the array
+            for (int i = 0; i < _upper; i++)
+            {
+                _array[i] = 0;   //initializing all the values to zero.
+            }
+            _numElements = 0;
+        }
+
+        //implementing the selection sort
+        public void SelectionSort()
+        {
+            //initializing a temporary variable
+            int temp, min;
+
+            //looping through the array
+            for (int outer = 0; outer <= _upper; outer++)
+            {
+                //assigning the minimum value as outer
+                min = outer;
+                
+                for (int inner = outer+1; inner <= _upper; inner++)
+                {
+                    if (_array[inner] < _array[min])
+                    {
+                        min = inner;
+                    }
+                    temp = _array[outer];
+                    _array[outer] = _array[min];
+                    _array[min] = temp;
+                }
+                //Display the elements
+                DisplayElements();
+            }
+        }
+    }
+}
--- ArrayTestBed/Program.cs
+++ ArrayTestBed/Program.cs
@@ -7,12 +7,14 @@ namespace ArrayTestBed
         public static void Main(string[] args)
         {
             var carray = new CArray(10);
+            var darray = new DArray(10);
             //Display the array elements;
 
             var rnd = new Random(100);
             for (int i = 0; i < 10; i++)
             {
                 carray.Insert((int)(rnd.NextDouble()*100));
+                darray.Insert((int)(rnd.NextDouble() * 100));
             }
             Console.WriteLine("Before sorting the array is: ");
             carray.DisplayElements();
@@ -24,6 +26,22 @@ namespace ArrayTestBed
             Console.WriteLine();
             Console.WriteLine("After Sorting.......");
             carray.DisplayElements();
+
+            Console.WriteLine("\n\n\n\n");
+            //getting the operations of selection sort
+            Console.WriteLine("Getting the operations of Selection Sort");
+
+            Console.WriteLine("Before sorting the array is: ");
+            darray.DisplayElements();
+
+            Console.WriteLine();
+            Console.WriteLine("During Sorting.......");
+            darray.SelectionSort();
+
+            Console.WriteLine();
+            Console.WriteLine("After Sorting.......");
+            darray.DisplayElements();
+
         }
     }
 }
